8f8dbb70c76382740fb62f632c1fba3f84c532d1
Vansh Arora <vansh0317.becse24@chitkara.edu.in>
2025-03-21 20:34:30 +0530

    Solution to Logical OR Operation

---
+++ Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+
+    scanf("%d %d",&a,&b);
+    
+    if (a>0 || b>0) {
+        printf("True");
+    } else {
+        printf("False");
+    }
+    return 0;
+    
+}
