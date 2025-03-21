964de6db451c664abca2399aba1e35f035959d79
Vansh Arora <vansh0317.becse24@chitkara.edu.in>
2025-03-21 20:41:06 +0530

    Solution to Logical AND and OR Combination

--- Logical AND and OR Combination@@67882a731b4392d8fc1577df/code.c
+++ Logical AND and OR Combination@@67882a731b4392d8fc1577df/code.c
@@ -8,7 +8,7 @@ int main() {
     
     if (a>0 && b<0) {
         printf("True");
-    } if else (a == 0 || b == 0) {
+    } else if (a == 0 || b == 0) {
         printf("True");
     } else {
         printf("False");
