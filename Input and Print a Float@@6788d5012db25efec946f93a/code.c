88d74bb7ca93fd7ce9a02a3b7f70552d6a472c37
Vansh Arora <vansh0317.becse24@chitkara.edu.in>
2025-03-21 14:17:23 +0530

    Solution to Input and Print a Float

--- Input and Print a Float@@6788d5012db25efec946f93a/code.c
+++ Input and Print a Float@@6788d5012db25efec946f93a/code.c
@@ -1,24 +1,12 @@
 #include <stdio.h>
 
-int add(int a, int b, int c) {
-    return a + b + c;
-}
+int main(){
+    float num;
 
-int product(int a, int b, int c) {
-    return a * b * c;
-}
+    printf("Enter a number : ");
+    scanf("%f\n" ,&num);
 
-int main() {
-    int num1, num2, num3, num4;
-    
-    printf("Enter four numbers: ");
-    scanf("%d %d %d %d", &num1, &num2, &num3, &num4);
-    
-    int sum = add(num1, num2, num3);
-    int prod = product(num1, num2, num3);
-    
-    printf("Sum of first three numbers: %d\n", sum);
-    printf("Product of first three numbers: %d\n", prod);
+    printf("You entered: ",num);
 
     return 0;
 }
