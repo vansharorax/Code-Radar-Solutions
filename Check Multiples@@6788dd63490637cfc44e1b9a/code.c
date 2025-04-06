d2bf9a2ef69fddc6fbcb494eccec8f7bdb22d1f6
Vansh Arora <vansh0317.becse24@chitkara.edu.in>
2025-04-06 16:01:33 +0530

    Solution to Check Multiples

---
+++ Check Multiples@@6788dd63490637cfc44e1b9a/code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int a;
+    int b;
+    scanf("%d %d",&a ,&b);
+    if ( a== 10 && b == 5) {
+        printf("Yes");
+    } else {
+        printf("No");
+    }
+    return 0;
+}
