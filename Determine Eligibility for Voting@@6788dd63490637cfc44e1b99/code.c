44013c2593c8b278341d7d8d583e533d5954594d
Vansh Arora <vansh0317.becse24@chitkara.edu.in>
2025-04-06 15:55:46 +0530

    Solution to Determine Eligibility for Voting

--- Determine Eligibility for Voting@@6788dd63490637cfc44e1b99/code.c
+++ Determine Eligibility for Voting@@6788dd63490637cfc44e1b99/code.c
@@ -3,7 +3,7 @@ int main() {
     int age;
     if (age >= 18) {
         printf("Eligible");
-    } else if {
-        printf("Not Eligible")
+    } else  {
+        printf("Not Eligible");
     }
 }
