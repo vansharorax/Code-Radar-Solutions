e6e2197889857bb24656756436591006ea5eb41a
Vansh Arora <vansh0317.becse24@chitkara.edu.in>
2025-04-06 15:24:37 +0530

    Solution to Grade Based on Marks

---
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int marks;
+    scanf("%d",&marks);
+
+    if (marks == '95') {
+        printf("A");
+    } else (marks == '95') {
+        printf("A");
+    } 
+}
