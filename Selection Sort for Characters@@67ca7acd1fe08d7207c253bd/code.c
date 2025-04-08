99a8bb2c422e84bafe4ccd2e5635f76cfe589539
Vansh Arora <vansh0317.becse24@chitkara.edu.in>
2025-04-08 19:41:23 +0530

    Solution to Selection Sort for Characters

--- Selection Sort for Characters@@67ca7acd1fe08d7207c253bd/code.c
+++ Selection Sort for Characters@@67ca7acd1fe08d7207c253bd/code.c
@@ -1,19 +1,44 @@
 #include <stdio.h>
-#include <string.h>
 
 #define MAX 100
 #define LEN 100
 
+// Manual string comparison function
+int compareStrings(char a[], char b[]) {
+    int i = 0;
+    while (a[i] != '\0' && b[i] != '\0') {
+        if (a[i] < b[i])
+            return -1;
+        else if (a[i] > b[i])
+            return 1;
+        i++;
+    }
+    if (a[i] == '\0' && b[i] == '\0')
+        return 0;
+    else if (a[i] == '\0')
+        return -1;
+    else
+        return 1;
+}
+
+// Manual string copy
+void copyString(char dest[], char src[]) {
+    int i = 0;
+    while ((dest[i] = src[i]) != '\0')
+        i++;
+}
+
 void selectionSort(char arr[][LEN], int n) {
     char temp[LEN];
-    for (int i = 0; i < n-1; i++) {
-        for (int j = i+1; j < n; j++) {
-            if (strcmp(arr[i], arr[j]) > 0) {
-                strcpy(temp, arr[i]);
-                strcpy(arr[i], arr[j]);
-                strcpy(arr[j], temp);
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (compareStrings(arr[i], arr[j]) > 0) {
+                copyString(temp, arr[i]);
+                copyString(arr[i], arr[j]);
+                copyString(arr[j], temp);
             }
         }
     }
 }
+
 
