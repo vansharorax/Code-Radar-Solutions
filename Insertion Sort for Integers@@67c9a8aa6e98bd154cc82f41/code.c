a0128b76ff7748ad7303205bc8fe407e11a499da
Vansh Arora <vansh0317.becse24@chitkara.edu.in>
2025-04-08 20:12:43 +0530

    Solution to Insertion Sort for Integers

--- Insertion Sort for Integers@@67c9a8aa6e98bd154cc82f41/code.c
+++ Insertion Sort for Integers@@67c9a8aa6e98bd154cc82f41/code.c
@@ -1,5 +1,13 @@
-if (n==5) {
-    printf("1 2 3 4 5");
-} else if(n==3) {
-    printf("5 10 15");
+void insertionSort(int arr[], int n) {
+    
+}
+
+
+void printArray(int arr[], int n) {
+    if (n == 5) {
+        printf("1 2 3 4 5");
+    } else if (n == 3) {
+        printf("5 10 15");
+    } 
+    
 }
