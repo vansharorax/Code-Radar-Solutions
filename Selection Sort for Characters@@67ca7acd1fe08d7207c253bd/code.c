8ec612ee0a337c723c11321903c7e9f920501d14
Vansh Arora <vansh0317.becse24@chitkara.edu.in>
2025-04-08 19:57:34 +0530

    Solution to Selection Sort for Characters

--- Selection Sort for Characters@@67ca7acd1fe08d7207c253bd/code.c
+++ Selection Sort for Characters@@67ca7acd1fe08d7207c253bd/code.c
@@ -1,50 +1,21 @@
-#include <stdio.h>
-
-void selectionSort(char arr[][100], int n);
-void printArray(char arr[][100], int n);
-int compareStrings(char s1[], char s2[]);
-void swapStrings(char s1[], char s2[]);
-
-
-
-void selectionSort(char arr[][100], int n) {
+void selectionSort(char arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
-        int min_idx = i;
+        int minIndex = i;
         for (int j = i + 1; j < n; j++) {
-            if (compareStrings(arr[j], arr[min_idx]) < 0) {
-                min_idx = j;
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
             }
         }
-        if (min_idx != i) {
-            swapStrings(arr[i], arr[min_idx]);
-        }
+
+        char temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
     }
 }
 
-int compareStrings(char s1[], char s2[]) {
-    int i = 0;
-    while (s1[i] != '\0' && s2[i] != '\0') {
-        if (s1[i] != s2[i]) {
-            return s1[i] - s2[i];
-        }
-        i++;
-    }
-    return s1[i] - s2[i];
-}
-
-void swapStrings(char s1[], char s2[]) {
-    char temp[100];
-    int i = 0;
-    while ((temp[i] = s1[i]) != '\0') i++;
-    i = 0;
-    while ((s1[i] = s2[i]) != '\0') i++;
-    i = 0;
-    while ((s2[i] = temp[i]) != '\0') i++;
-}
-
-void printArray(char arr[][100], int n) {
+void printArray(char arr[], int n) {
     for (int i = 0; i < n; i++) {
-        printf("%s ", arr[i]);
+        printf("%c ", arr[i]);
     }
     printf("\n");
 }
